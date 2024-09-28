7b55ab65ea3c2bf5e6834d290e1533549b2faf6d
dtwan0212 <toando0212@gmail.com>
2024-09-28 16:22:52 +0700

    lab 11

--- app/src/main/java/vn/edu/usth/usthweather/WeatherActivity.java
+++ app/src/main/java/vn/edu/usth/usthweather/WeatherActivity.java
@@ -1,31 +1,83 @@
 package vn.edu.usth.usthweather;
 
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.tabs.TabLayout;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 
 public class WeatherActivity extends AppCompatActivity {
-
+    private static final int REQUEST_WRITE_STORAGE = 112;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
         Log.i("WeatherActivity", "onCreate");
 
-        ViewPager viewPager = findViewById(R.id.viewPager);
-        TabLayout tabLayout = findViewById(R.id.tabLayout);
+        boolean hasPermission = (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED);
+        if (!hasPermission) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    REQUEST_WRITE_STORAGE);
+        } else {
+            extractAndPlayMP3();
+        }
 
-        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
-        adapter.addFragment(new WeatherAndForecastFragment(), "HANOI, VIETNAM");
-        adapter.addFragment(new WeatherAndForecastFragment(), "PARIS, FRANCE");
-        adapter.addFragment(new WeatherAndForecastFragment(), "TOULOUSE, FRANCE");
-        viewPager.setAdapter(adapter);
+//        ViewPager viewPager = findViewById(R.id.viewPager);
+//        TabLayout tabLayout = findViewById(R.id.tabLayout);
+//
+//        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+//        adapter.addFragment(new WeatherAndForecastFragment(), "HANOI, VIETNAM");
+//        adapter.addFragment(new WeatherAndForecastFragment(), "PARIS, FRANCE");
+//        adapter.addFragment(new WeatherAndForecastFragment(), "TOULOUSE, FRANCE");
+//        viewPager.setAdapter(adapter);
+//
+//        tabLayout.setupWithViewPager(viewPager);
+    }
 
-        tabLayout.setupWithViewPager(viewPager);
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_WRITE_STORAGE && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            extractAndPlayMP3();
+        }
+    }
+
+    private void extractAndPlayMP3() {
+        File sdcard = Environment.getExternalStorageDirectory();
+        File file = new File(sdcard, "music.mp3");
+
+        try (InputStream in = getResources().openRawResource(R.raw.audio);
+             FileOutputStream out = new FileOutputStream(file)) {
+
+            byte[] buffer = new byte[1024];
+            int read;
+            while ((read = in.read(buffer)) != -1) {
+                out.write(buffer, 0, read);
+            }
+
+            // Play the MP3 file
+            MediaPlayer mediaPlayer = new MediaPlayer();
+            mediaPlayer.setDataSource(file.getPath());
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
